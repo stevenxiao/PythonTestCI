96e40b5ac44d3dbb4303b598fa5f9ed06a5cb68f
steven <xiaoxi328@gamil.com>
2017-08-15 14:17:40 +0800

    init repo

---
+++ src/add.py
@@ -0,0 +1,7 @@
+#! -*- coding=utf-8 -*-
+
+def add(x, y):
+    return x+y
+
+if __name__ == '__main__':
+    print add(2, 3)
---
+++ test/add_test.py
@@ -0,0 +1,22 @@
+#! coding=utf-8
+
+import unittest
+import os
+import sys
+
+CUR_PATH = os.path.abspath(os.path.curdir)
+sys.path.append(CUR_PATH)
+
+from src.add import add
+
+class UniTestCoverage(unittest.TestCase):
+    def test_add_equal(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertNotEqual(add(2, 4), 7)
+
+
+    def test_add_notequal(self):
+        self.assertNotEqual(add(2, 4), 6)
+
+if __name__ == "__main__":
+    unittest.main()
